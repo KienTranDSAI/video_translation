49f152cda4b0525c327ac9be79707517a0532c4f
KienTran <kientran.dsai@gmail.com>
2024-05-21 08:32:41 +0700

    Fix get_code.sh

--- get_code.sh
+++ get_code.sh
@@ -1,3 +1,14 @@
 git lfs install
 git clone https://huggingface.co/vinai/PhoWhisper-medium
 git clone https://huggingface.co/vinai/vinai-translate-vi2en-v2
+
+git clone https://github.com/neonbjb/tortoise-tts.git
+
+cd tortoise-tts
+python setup.py install
+pip install -r requirements.txt
+cd ../
+git clone https://github.com/soumik-kanad/diff2lip.git
+cd diff2lip
+pip install -r requirements.txt
+cd ../
